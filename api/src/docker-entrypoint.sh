ada95d0ceab18eadd0a7f1ca6ceed0f22ef4dede
vicky <vickycp417@gmailcom>
2025-06-16 14:53:29 +0530

    v2

--- api/src/docker-entrypoint.sh
+++ api/src/docker-entrypoint.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-CGROUP_FS="/sys/fs/cgroup"
-if [ ! -e "$CGROUP_FS" ]; then
-  echo "Cannot find $CGROUP_FS. Please make sure your system is using cgroup v2"
-  exit 1
-fi
-
-if [ -e "$CGROUP_FS/unified" ]; then
-  echo "Combined cgroup v1+v2 mode is not supported. Please make sure your system is using pure cgroup v2"
-  exit 1
-fi
+CGROUP_FS="/tmp/cgroup"
+mkdir -p $CGROUP_FS
 
 if [ ! -e "$CGROUP_FS/cgroup.subtree_control" ]; then
-  echo "Cgroup v2 not found. Please make sure cgroup v2 is enabled on your system"
-  exit 1
+  echo "Creating cgroup v2 structure in $CGROUP_FS"
+  mkdir -p "$CGROUP_FS/isolate/init"
 fi
 
-cd /sys/fs/cgroup && \
-mkdir isolate/ && \
+cd $CGROUP_FS && \
 echo 1 > isolate/cgroup.procs && \
 echo '+cpuset +cpu +io +memory +pids' > cgroup.subtree_control && \
 cd isolate && \
-mkdir init && \
 echo 1 > init/cgroup.procs && \
-echo '+cpuset +memory' > cgroup.subtree_control && \
+echo '+cpuset +memory' > cgroup.subtree_control
+
 echo "Initialized cgroup" && \
-chown -R piston:piston /piston && \
+chown -R piston:piston /tmp/piston && \
 exec su -- piston -c 'ulimit -n 65536 && node /piston_api/src'
