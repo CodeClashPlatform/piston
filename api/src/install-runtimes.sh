1077d4109cfe436f6594349ac71a4fab95b87b98
vicky <vickycp417@gmailcom>
2025-06-16 15:56:30 +0530

    afets v1 32

--- api/src/install-runtimes.sh
+++ api/src/install-runtimes.sh
@@ -5,25 +5,22 @@ log() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
 }
 
-# Set working directory
-cd /piston_api
-
 log "Starting runtime installations..."
 
 # Create packages directory if it doesn't exist
 mkdir -p /piston/packages
 
-# Install runtimes
+# Install runtimes using global ppman
 log "Installing Python runtime..."
-node cli/index.js ppman install python@3.9.0
+ppman install python@3.9.0
 log "Installing Java runtime..."
-node cli/index.js ppman install java@15.0.2
+ppman install java@15.0.2
 log "Installing Node.js runtime..."
-node cli/index.js ppman install nodejs@15.0.0
+ppman install nodejs@15.0.0
 
 log "Setting permissions..."
 chown -R piston:piston /piston/packages
 
 log "Runtime installations completed"
 log "Installed runtimes:"
-node cli/index.js ppman list
+ppman list
