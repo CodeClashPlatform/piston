aecc2d56e809a7a7663669695f029b652c73a7fa
vicky <vickycp417@gmailcom>
2025-06-16 18:50:29 +0530

    uehje

--- api/src/install-runtimes.sh
+++ api/src/install-runtimes.sh
@@ -10,23 +10,25 @@ log "Starting runtime installations..."
 mkdir -p /piston/packages
 chmod 755 /piston/packages
 
-# Install runtimes using global CLI
+# Set paths
 export PATH="/usr/local/bin:$PATH"
 export NODE_PATH=/usr/local/lib/node_modules
 
 cd /piston_api
 
 log "Installing Python runtime..."
-piston-cli ppman install python=3.9.4
+piston ppman install python=3.9.4
 
 log "Installing Node.js runtime..."
-piston-cli ppman install nodejs=16.14.0
+piston ppman install nodejs=16.14.0
 
 log "Installing Java runtime..."
-piston-cli ppman install java=17.0.2
+piston ppman install java=17.0.2
 
 log "Setting permissions..."
 chown -R piston:piston /piston/packages
 chmod -R 755 /piston/packages
 
 log "Runtime installations completed"
+log "Installed runtimes:"
+piston ppman list
