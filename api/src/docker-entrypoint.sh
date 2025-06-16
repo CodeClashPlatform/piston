fd94c5160cf432e0488136f0319a4077a0c31361
vicky <vickycp417@gmailcom>
2025-06-16 14:59:17 +0530

    v123

--- api/src/docker-entrypoint.sh
+++ api/src/docker-entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Set up required directories
 CGROUP_FS="/tmp/cgroup"
+PISTON_DIR="/tmp/piston"
+
+# Create necessary directories
 mkdir -p $CGROUP_FS
+mkdir -p $PISTON_DIR
 
 if [ ! -e "$CGROUP_FS/cgroup.subtree_control" ]; then
   echo "Creating cgroup v2 structure in $CGROUP_FS"
   mkdir -p "$CGROUP_FS/isolate/init"
 fi
 
+# Setup cgroups
 cd $CGROUP_FS && \
 echo 1 > isolate/cgroup.procs && \
 echo '+cpuset +cpu +io +memory +pids' > cgroup.subtree_control && \
@@ -15,6 +21,10 @@ cd isolate && \
 echo 1 > init/cgroup.procs && \
 echo '+cpuset +memory' > cgroup.subtree_control
 
-echo "Initialized cgroup" && \
-chown -R piston:piston /tmp/piston && \
+# Set proper permissions
+echo "Initializing directories and permissions..."
+chown -R piston:piston $PISTON_DIR
+echo "Initialized cgroup and permissions"
+
+# Start the application
 exec su -- piston -c 'ulimit -n 65536 && node /piston_api/src'
